1262a1f9582a9ffb7f53a1fd8fee6fd73889598f
CauanTeixeira <cauan.jesus.ct@outlook.com>
2022-11-07 18:00:36 -0300

    cameço

---
+++ POO/control.php
@@ -0,0 +1,38 @@
+<?php
+$control=[
+    'user1' => [
+        'User' => 'cauan@email.com',
+        'pass' => '1234',
+        'name' => 'Silas Kando',
+        'cpf' => '666.777.222-54',
+        // 'img' => 'img/user1.png'
+    ],
+    'user2' => [
+        'User' => 'revolucao@outlook.com',
+        'pass' => '157cm',
+        'name' => 'Napoleão Bonaparte',
+        'cpf' => '111.111.111-11',
+        // 'img' => 'img/user2.png'
+    ],
+    'user3' => [
+        'User' => 'mementomori@gmail.com',
+        'pass' => '2604121',
+        'name' => 'Marco Aurélio',
+        'cpf' => '123.456.789-00',
+        // 'img' => 'img/user3.png'
+    ],
+    'user4' => [
+        'User' => 'braganza@mail.com',
+        'pass' => 'corsarebaixado',
+        'name' => 'Dom Pedro II',
+        'cpf' => '777.777.777-77',
+        // 'img' => 'img/user4.png'
+    ],
+    'user5' => [
+        'User' => 'birmingham@mail.com',
+        'pass' => 'cenoura',
+        'name' => 'Thomas Shelby',
+        'cpf' => '593.837.613-44',
+        // 'img' => 'img/user5.png'
+    ]
+] ;
---
+++ POO/pagina1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pagina 1 </title>
+</head>
+<body>
+    <p>Email:</p>
+    <p>Nome:</p>
+    <p>CPF:</p> <br><br>
+    <h2>Insera os numeros da equação de 2 grau:</h2>
+    <form action="pagina2.php" method="get">
+        <label>Valor de A:</label><br>
+        <input type="number" name="A" id=""><br>
+        <label>Valor de B:</label><br>
+        <input type="number" name="B" id=""><br>
+        <label>Valor de C:</label><br>
+        <input type="number" name="C" id=""><br>
+        <button type="submit">Calcular</button>
+    </form>
+</body>
+</html>
